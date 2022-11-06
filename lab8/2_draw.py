ec2c418f72a743692c6f2dd80fd7890a2e716856
Annas129 <104022465+Annas129@users.noreply.github.com>
2022-11-06 23:16:40 +0300

    Update 2_draw.py

--- lab8/2_draw.py
+++ lab8/2_draw.py
@@ -1,10 +1,9 @@
 import pygame
-from pygame.draw import *
 from random import randint
-pygame.init()
+from PIL import Image, ImageDraw
 
-FPS = 1
-screen = pygame.display.set_mode((1200, 900))
+pygame.init()
+window = pygame.display.set_mode((1200, 700))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -14,58 +13,88 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-
-x1 = randint(100, 1100)
-y1 = randint(100, 900)
-r1 = randint(10, 100)
-x2 = randint(100, 1100)
-y2 = randint(100, 900)
-r2 = randint(10, 100)
 sum = 0
 
-def new_ball():
-    '''рисует заданное количество(до 2-х) новых шариков '''
-    global x1, y1, r1
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x1, y1), r1)
-    global x2, y2, r2
-    x2 = randint(100, 1100)
-    y2 = randint(100, 900)
-    r2 = randint(10, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x2, y2), r2)
+
+class Circle():
+    def __init__(self):
+        self.x = randint(100, 1100)
+        self.y = randint(100, 600)
+        self.radius = randint(10, 100)
+        self.vel_x = 1
+        self.vel_y = 1
+        self.color = COLORS[randint(0, 5)]
+
+class Rectangle():
+    def __init__(self):
+        self.x = randint(100, 1100)
+        self.y = randint(100, 600)
+        self.a = randint(10, 100)
+        self.b = randint(10, 100)
+        self.vel_y = 1
+        self.color = COLORS[randint(0, 5)]
+    def rotate(self, angle):
+        self.rect = pygame.transform.rotate(self.rect, angle)
 
 def click(event):
     '''Добавляет к сумме попаданий 1, если пользователь попал в шарик '''
     event.x, event.y = event.pos
     global sum
-    if ((((event.x - x1)**2 + (event.y - y1)**2) < r1**2) or (((event.x - x2)**2 + (event.y - y2)**2) < r2**2)):
+    if ((((event.x - ball1.x)**2 + (event.y - ball1.y)**2) < ball1.radius**2) or (((event.x - ball2.x)**2 + (event.y - ball2.y)**2) < ball2.radius**2)):
         sum+=1
+    if (event.x - rec.x) < rec.a and (event.y - rec.y) < rec.b:
+        sum+=2
 
-def movement():
-    dt = clock.tick(50) / 1000.0
-    vx = 10
-    vy = 10
-    global x1, y1
-    x1 += vx * dt
-    y1 += vy * dt
+print('За попадание в шар дается одно очко, за попадание в прямоугольник - два очка. Чтобы начать игру. нажмите пробел')
+ball1 = Circle()
+ball2 = Circle()
+rec = Rectangle()
 
-pygame.display.update()
+run = True
+start = False
 clock = pygame.time.Clock()
-finished = False
 
-while not finished:
-    clock.tick(FPS)
+while run:
+    clock.tick(120)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            finished = True
+            run = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             click(event)
-    movement()
-    new_ball()
-    pygame.display.update()
-    screen.fill(BLACK)
 
-print("Вы попали ", sum, " раз(а)")
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        start = True
+
+    bounds = window.get_rect()
+    if start:
+        ball1.y -= ball1.vel_y
+        ball1.x += ball1.vel_x
+        ball2.y -= ball2.vel_y
+        ball2.x += ball2.vel_x
+        rec.y += rec.vel_y
+
+        if ball1.x - ball1.radius < bounds.left or ball1.x + ball1.radius > bounds.right:
+            ball1.vel_x *= -1
+        if ball1.y - ball1.radius < bounds.top or ball1.y + ball1.radius > bounds.bottom:
+            ball1.vel_y *= -1
+
+        if ball2.x - ball2.radius < bounds.left or ball2.x + ball2.radius > bounds.right:
+            ball2.vel_x *= -1
+        if ball2.y - ball2.radius < bounds.top or ball2.y + ball2.radius > bounds.bottom:
+            ball2.vel_y *= -1
+
+        if rec.y - rec.b < bounds.top or rec.y + rec.b > bounds.bottom:
+            rec.vel_y *= -1
+
+    window.fill((0,0,0))
+    pygame.draw.rect(window, (255, 0, 0), bounds, 1)
+    pygame.draw.circle(window, ball1.color, (ball1.x, ball1.y), ball1.radius)
+    pygame.draw.circle(window, ball2.color, (ball2.x, ball2.y), ball2.radius)
+    pygame.draw.rect(window, rec.color, (rec.x, rec.y, rec.a, rec.b))
+    pygame.display.update()
+
+print("Вы набрали ", sum, " очка(ов)")
 
 pygame.quit()
